539137c3057760b69f284e036e67e55c8a7ff7ae
huyphung1602 <huy.phung.sw@gmail.com>
2026-04-18 15:24:55 +0700

    Shorten the page name

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Anti-Complexity",
+    pageTitle: "SWD Wiki",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
